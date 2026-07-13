e451598893d7be6073c85bed1735d7648202bf75
Kalpajit Bepary <kalpajitbepary@gmail.com>
2026-07-13 22:54:51 +0530

    Time: 289 ms (43%), Space: 20.5 MB (70.04%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+        
+        queue = deque([(beginWord, 1)])
+        
+        while queue:
+            word, steps = queue.popleft()
+            
+            if word == endWord:
+                return steps
+                
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    next_word = word[:i] + c + word[i+1:]
+                    if next_word in wordSet:
+                        wordSet.remove(next_word)
+                        queue.append((next_word, steps + 1))
+                        
+        return 0
